33a6cf5c30778a0a7edd12271ccb6aac4c4a2b75
jvmaiaa <joaovictormaia482@gmail.com>
2024-01-12 14:36:11 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		upadateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void upadateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());		
+	}
 }
